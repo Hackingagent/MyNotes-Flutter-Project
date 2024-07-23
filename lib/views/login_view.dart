df4e3e8b8d737856b9cd9b2ae854011b1e327ba2
Ngulefac Terence <ngulefac@kali.ngulefac>
2024-07-23 10:30:36 +0100

    step 3

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 //Type "stl" to create a widget statefull(stf) or stateless
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -74,7 +76,7 @@ class _LoginViewState extends State<LoginView> {
                   password: password,
                 );
                 Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/notes/',
+                  notesRoute,
                   (route) => false,
                 );
               } on FirebaseAuthException catch (e) {
@@ -93,7 +95,7 @@ class _LoginViewState extends State<LoginView> {
             onPressed: () {
               //Moving to the register view using the route created in the main function
               Navigator.of(context)
-                  .pushNamedAndRemoveUntil('/register/', (route) => false);
+                  .pushNamedAndRemoveUntil(registerRoute, (route) => false);
             },
             child: const Text('Not registered yet? register here'),
           )
