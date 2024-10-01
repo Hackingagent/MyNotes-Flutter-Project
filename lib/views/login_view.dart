e0dfff1cdb698feb409c45fe009ec08369c8c061
Ngulefac Terence <ngulefac@kali.ngulefac>
2024-10-01 21:46:23 +0100

    step 9

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
 
 //Type "stl" to create a widget statefull(stf) or stateless
@@ -70,12 +71,12 @@ class _LoginViewState extends State<LoginView> {
               final password = _password.text;
 
               try {
-                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                await AuthService.firebase().logIn(
                   email: email,
                   password: password,
                 );
-                final user = FirebaseAuth.instance.currentUser;
-                if (user?.emailVerified ?? false) {
+                final user = AuthService.firebase().currentUser;
+                if (user?.isEmailVerified ?? false) {
                   //user email is verified
 
                   Navigator.of(context).pushNamedAndRemoveUntil(
@@ -84,28 +85,21 @@ class _LoginViewState extends State<LoginView> {
                   );
                 } else {
                   //users email is not verified
-                  await user?.sendEmailVerification();
+
                   Navigator.of(context).pushNamedAndRemoveUntil(
                     verifyEmailRoute,
                     (route) => false,
                   );
                 }
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'invalid-credential') {
-                  await showErrorDialog(
-                    context,
-                    'User Not Found',
-                  );
-                } else {
-                  await showErrorDialog(
-                    context,
-                    'Error: ${e.code}', //putting a variable inside a string inside ""
-                  );
-                }
-              } catch (e) {
+              } on InvalidCredentialAuthException {
                 await showErrorDialog(
                   context,
-                  e.toString(),
+                  'User Not Found',
+                );
+              } on GenericAuthException {
+                await showErrorDialog(
+                  context,
+                  'Authentication Error', //putting a variable inside a string inside ""
                 );
               }
             },
