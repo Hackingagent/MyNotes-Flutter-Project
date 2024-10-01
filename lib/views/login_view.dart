5f038a09abf2bc896f7e9658f8c0ed4c66f8b904
Ngulefac Terence <ngulefac@kali.ngulefac>
2024-10-01 20:57:58 +0100

    step 8

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -74,10 +74,22 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  notesRoute,
-                  (route) => false,
-                );
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  //user email is verified
+
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    notesRoute,
+                    (route) => false,
+                  );
+                } else {
+                  //users email is not verified
+                  await user?.sendEmailVerification();
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    verifyEmailRoute,
+                    (route) => false,
+                  );
+                }
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'invalid-credential') {
                   await showErrorDialog(
